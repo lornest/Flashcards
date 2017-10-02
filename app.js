a0fcd5d479bdf0f81c989c0849e6048c89ff3e49
lornestalker <lorne_stalker@yahoo.co.uk>
2017-10-02 17:43:37 -0400

    initial commit from 40,000 feet

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+// the express(); function creates a new express application
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes')
+const cardRoutes = require('./routes/cards')
+
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+app.use((req, res, next) => {
+    console.log("Hello I'm middleware");
+    const err = new Error("I'm an error");
+    err.status = 500;
+    next(err);
+  });
+
+app.use((err, req, res, next) => {
+  const notFound = new Error('Not Found');
+  notFound.status = 404;
+  next(notFound);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
+
+// to set up a development server, call the .listen(port); function
+app.listen(3000, () => {
+  console.log("The application is running on localhost:3000");
+});
---
+++ routes/cards.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
+
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length;
+  const randomId = Math.floor( Math.random() * numberOfCards );
+  res.redirect(`/cards/${randomId}`)
+});
+
+router.get('/:id', (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+
+  if ( !side ) {
+    return res.redirect(`/cards/${id}?side=question`);
+  }
+
+  const name = req.cookies.username;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+
+  const templateData = { id, text, name };
+
+  if (side === 'question') {
+    templateData.hint = hint;
+    templateData.flipTo = 'answer';
+    templateData.flipToDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.flipTo = 'question';
+    templateData.flipToDisplay = 'Question';
+  }
+
+  res.render("card", templateData);
+});
+
+module.exports = router;
